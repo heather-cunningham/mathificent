41f5bd11fea7e625911d5d68a5ffa52ce6110be3
heather-irene <cunningham.heatherirene@gmail.com>
2024-12-24 15:39:29 -0500

    vue-routing: In main.js, created routes and router, and set app to use router. Created two new skeletons of components to house config & play views -- not done yet.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
 import {createApp} from 'vue';
 import App from './App.vue';
+import {createRouter, createWebHashHistory} from "vue-router";
+import GameConfig from "@/components/GameConfig.vue";
+import GamePlay from "@/components/GamePlay.vue";
 
-createApp(App).mount('#app');
+const routes = [
+  {
+    path: "/",
+    component: GameConfig
+  },
+  {
+    path: "/play/:operation/:maxNumber",
+    component: GamePlay,
+    props: true
+  },
+];
+// createRouter() takes in an Obj with properties:
+// `history:` assigned to the history type, in our case `createWebHashHistory`.
+// And, the routes array.
+const router = createRouter(
+  {
+    history: createWebHashHistory(),
+    routes,
+  }
+);
+const app = createApp(App);
+
+app.use(router);
+app.mount('#app');
